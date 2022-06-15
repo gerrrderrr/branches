b11b10a1d0a4fbff489cf790116e73ed92ead410
Olga Kozlova <o.o.olga.kozlova@gmail.com>
2022-06-15 13:14:51 +0300

    Added Main and Flute

---
+++ Flute.java
@@ -0,0 +1,6 @@
+class Flute {
+	private String song;
+	public Flute(String song) {
+		this.song = song;
+	}
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+class Main {
+	public static void main(String[] args) {
+		System.out.println("How are you doing?");
+		Flute p1 = new Flute("Let's sing together");
+		System.out.println(p1);
+	}
+}
